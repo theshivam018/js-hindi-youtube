0a08dad7afd9feb354378f5e4614669aff218fcd
shivam <shivamdigambarjha01@gmail.com>
2025-03-18 11:04:09 +0000

    conversion of datatypes

--- .devcontainer/01-basics/03-conversionoperation.js
+++ .devcontainer/01-basics/03-conversionoperation.js
@@ -1,3 +1,5 @@
+
+//one can either true or false aree called boolean
 let score = "hitesh"
 
 //console.log(typeof score);
@@ -44,7 +46,7 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+//console.log(str3);
 
 // console.log("1" + 2);
 // console.log(1 + "2");
@@ -56,8 +58,7 @@ let str3 = str1 + str2
 // console.log(+true);
 // console.log(+"");
 
-let num1, num2, num3
-
+let num1, num2, num3;
 num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
