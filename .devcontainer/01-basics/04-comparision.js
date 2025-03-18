d0d59f17167275af5fc366546977fc9271ee9567
shivam <shivamdigambarjha01@gmail.com>
2025-03-18 11:30:15 +0000

    comparison of datatype

---
+++ .devcontainer/01-basics/04-comparision.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);//avoid this types of conversion
+
+// console.log(null > 0); //avoid this types of conversion 
+// console.log(null >= 0);
+// console.log(null == 0); 
+
+// console.log(0 >= undefined);
+// console.log(0 == undefined);//avoid this types of conversion
+// console.log(0 > undefined);
+
+//===
+console.log("2" ===2);
